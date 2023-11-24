bd8c74088577768273c319224c52b7cb0d977e48
KooTaehyeon <vcro12123@naver.com>
2023-11-24 18:14:51 +0900

    [add] 렌더링 조건 변경 re9

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -330,13 +330,14 @@ const ProductDetail: NextPageWithLayout<Props> = ({ initialData }) => {
         {data && isTasting && (
           <div className='flex  flex-col items-center bg-[url("/assets/icons/common/tasting-bg.png")]'>
             <div className=' mt-10 items-center text-[16px] font-bold'>피쉬 테이스팅 노트</div>
-            <Chat data={data.tastingNoteInfo} />
+            <Chat data={data?.tastingNoteInfo ?? []} />
             <TastingInfo
-              keyword={data.tastingNoteInfo[0]?.textures ?? []}
+              keyword={data?.tastingNoteInfo[0]?.textures ?? []}
               info={{
-                difficultyLevelOfTrimming: data.tastingNoteInfo[0]?.difficultyLevelOfTrimming ?? '',
-                recommendedCookingWay: data.tastingNoteInfo[0]?.recommendedCookingWay ?? '',
-                theScentOfTheSea: data.tastingNoteInfo[0]?.theScentOfTheSea ?? '',
+                difficultyLevelOfTrimming:
+                  data?.tastingNoteInfo[0]?.difficultyLevelOfTrimming ?? '',
+                recommendedCookingWay: data?.tastingNoteInfo[0]?.recommendedCookingWay ?? '',
+                theScentOfTheSea: data?.tastingNoteInfo[0]?.theScentOfTheSea ?? '',
               }}
             />
           </div>
