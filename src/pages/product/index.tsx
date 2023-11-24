346912e05909046cb76a75f41c0cdcea04f8751e
KooTaehyeon <vcro12123@naver.com>
2023-11-24 17:53:24 +0900

    [add] 렌더링 조건 변경 re5

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -322,7 +322,7 @@ const ProductDetail: NextPageWithLayout<Props> = ({ initialData }) => {
         />
         {/* <ProductCompare /> */}
         {/* BARO’s 피쉬 노트 */}
-        {data && data.tastingNoteInfo.length > 0 && (
+        {data && data?.tastingNoteInfo?.length > 0 && (
           <div className='flex  flex-col items-center bg-[url("/assets/icons/common/tasting-bg.png")]'>
             <div className=' mt-10 items-center text-[16px] font-bold'>피쉬 테이스팅 노트</div>
             <Chat data={data.tastingNoteInfo} />
