619c07485b5b075b187cd823778e2155aeb01bac
KooTaehyeon <vcro12123@naver.com>
2023-11-28 16:19:21 +0900

    [test]  ptr test2

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,4 +1,4 @@
-import { type ComponentProps, useState } from 'react';
+import { type ComponentProps } from 'react';
 import { Footer, type FooterProps } from 'src/components/common/footer';
 import { Header, type HeaderProps } from 'src/components/common/header';
 import cm from 'src/utils/class-merge';
