95e835da22c4e505e0af11d03dac0d0d032f1438
KooTaehyeon <vcro12123@naver.com>
2023-11-30 17:53:15 +0900

    [fix] 모달창 오픈시 pc 배너 안나오는 현상 수정

--- src/components/product/bottom-sheet.tsx
+++ src/components/product/bottom-sheet.tsx
@@ -199,9 +199,10 @@ const BottomSheet = ({ data, isVisible, setIsVisible }: Props) => {
     if (!isVisible) return;
     document.body.style.cssText = `
       position: fixed; 
-      // top: -${window.scrollY}px;
-      overflow-y: scroll;
-      width: 100%;`;
+      top: -${window.scrollY}px;
+      width: 100%;
+      height: 100%;
+      `;
     return () => {
       const scrollY = document.body.style.top;
       document.body.style.cssText = '';
