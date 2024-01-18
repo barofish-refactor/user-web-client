0bea4fed574ee44832b029eb687146809a677ae0
KooTaehyeon <vcro12123@naver.com>
2024-01-18 18:02:01 +0900

    [fix] 로그인 에러상황 핸들링2

--- src/components/review/newItem.tsx
+++ src/components/review/newItem.tsx
@@ -3,7 +3,7 @@ import { FreeMode } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { getCookie } from 'cookies-next';
+import { deleteCookie, getCookie } from 'cookies-next';
 import { useRouter } from 'next/router';
 import { client } from 'src/api/client';
 
@@ -25,7 +25,7 @@ interface Props {
   showInfo?: boolean;
   refetch: () => void;
 }
-
+const { ACCESS_TOKEN, REFRESH_TOKEN } = VARIABLES;
 /** 구매후기 */
 export function NewReviewItem({ data, isMine, showInfo = true, refetch }: Props) {
   const router = useRouter();
@@ -45,6 +45,19 @@ export function NewReviewItem({ data, isMine, showInfo = true, refetch }: Props)
     if (res.data.isSuccess) {
       return res.data.data;
     } else {
+      if (res.data.code === '101' || res.data.code === '102') {
+        setAlert({ message: res.data.errorMsg ?? '' });
+        router.replace('/login');
+        return;
+      } else if (res.data.code === '103') {
+        deleteCookie(ACCESS_TOKEN);
+        deleteCookie(REFRESH_TOKEN);
+        setAlert({ message: res.data.errorMsg ?? '' });
+        router.replace('/login');
+        return;
+      }
+      console.log(res.data.errorMsg);
+      //
       throw new Error(res.data.errorMsg);
     }
   });
